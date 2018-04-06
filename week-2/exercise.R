59901e56b7dd3326d8837ed9789f897e6f500df4
Ryan Williams <rmw698@uw.edu>
2018-04-06 15:25:34 -0700

    complete week 2 lab exercise

--- week-2/exercise.R
+++ week-2/exercise.R
@@ -30,24 +30,41 @@ said_the_famous <- paste(my_line, " - ", initial)
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
-
+numbers <- c(1, 8, 5, 4, 8, 2)
+typeof(numbers)
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(vec1, vec2) {
+  diff_length <- length(vec1) - length(vec2)
+  diff_length <- abs(diff_length)
+  print(paste("The difference in lengths is", diff_length))
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
-
+letters <- c("a", "c", "z", "f")
+CompareLength(numbers, letters)
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDifference <- function(vec1, vec2) {
+  dif <- length(vec1) - length(vec2)
+  if(dif >= 0) {
+    print(paste("Your first vector is longer by", dif, "elements"))
+  }
+  else {
+    print(paste("Your second vector is longer by", abs(dif), "elements"))
+  }
+}
 # Pass two vectors to your `DescribeDifference` function
-
+DescribeDifference(letters, numbers)
 
 # Write a function `CombineVectors` that takes in 3 vectors and combines them into one
-
+CombineVectors <- function(vec1, vec2, vec3) {
+  vec_combine <- c(vec1, vec2, vec3)
+  vec_combine
+}
 # Send 3 vectors to your function to test it.
-
+words <- c("dog", "car", "bottle")
+CombineVectors(numbers, letters, words)
 # Write a function `CapsTime` that takes in a vector of names of courses you're taking, ex "Informatics 201" and removes all capital letters
 
